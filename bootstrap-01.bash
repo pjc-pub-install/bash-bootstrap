d677cf7948c5eba48f658e0e71821f51355b6475
pjc <pjc@lp21>
2022-04-08 10:33:59 -0700

    init

---
+++ bootstrap-01.bash
@@ -0,0 +1,19 @@
+
+# you should install mc before you do anything
+sudo apt-get update
+sudo install mc
+
+
+# you need pip before you can install ansible
+# ref:
+#   https://linuxize.com/post/how-to-install-pip-on-ubuntu-20.04/
+sudo apt-get update
+sudo -H apt-get install python3-pip
+
+
+# eget is a marvellous single binary installer from the maker of micro
+# src:
+#   [source](https://github.com/zyedidia/eget)
+curl https://zyedidia.github.io/eget.sh | sh
+
+
