c7ee10b25fae872f07741346dfa19ca1537a7804
pjc <pjc@lp21>
2022-04-09 08:20:12 -0700

    added simple main.bash, the entry point for the bash script

---
+++ main.bash
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+
+echo '
+#####################################################################
+#####################################################################
+
+	main.bash
+
+
+	wget https://raw.githubusercontent.com/username/repository/branch/path/filename.md
+
+	wget https://raw.githubusercontent.com/pjc-pub-bootstrap/master/path/bootstrap-01.bash
+
+	https://raw.githubusercontent.com/pjc-pub-install/bash-bootstrap/master/bootstrap-01.bash
+
+#####################################################################
+#####################################################################
+'
